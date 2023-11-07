813d53a397d6fbb3ed7822bd95a2a4329bd860b7
kevin <kevinup13@gmail.com>
2023-11-07 01:47:06 -0400

    update slider

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,24 @@ document.body.onresize = () => {
         removeDisplay();
     }
 }
+
+//slides
+
+let slideIndex = 1;
+mostrarSlide(slideIndex);
+
+function mostrarSlide(n) {
+
+    let slides = document.getElementsByClassName("slide");
+    let thumbnails = document.getElementsByClassName("thumbnail");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].classList.remove("active");
+    }
+    slides[n - 1].style.display = "block";
+    thumbnails[n - 1].classList.add("active");
+}
